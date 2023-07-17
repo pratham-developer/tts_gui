afaa5f8df9238127327241f59c4a273a326d92c1
Pratham Khanduja <73526443+pratham-developer@users.noreply.github.com>
2023-07-17 16:36:02 +0530

    Add files via upload

---
+++ tts_gui_version4.py
@@ -0,0 +1,83 @@
+import tkinter as tk
+from tkinter import ttk
+import os
+from tkinter import filedialog as fd
+from gtts import gTTS
+from tkinter import messagebox as mb
+from langdetect import detect
+import socket
+root = tk.Tk()
+root.geometry("565x435")
+root.title("Text to Speech")
+root.resizable(width=False,height=False)
+sc=ttk.Scrollbar(root)
+sc.pack(side="right", fill="y")
+enter = tk.Text(height=21, width=60, yscrollcommand=sc.set, font=("Arial", 15))
+enter.pack(side="top")
+sc.config(command=enter.yview)
+
+choose = ttk.Combobox(root,values=["India","US","UK","Canada","Australia","Ireland"])
+choose.place(x=340,y=403)
+choose.current(0)
+choose['state']='readonly'
+def hit():
+    global loc
+    try:
+      tex = enter.get("1.0", "end-1c")
+      file = gTTS(text=tex, lang=detect(tex))
+      if detect(tex)=='en':
+          acc = choose.get()
+          if acc =="India":
+              way="co.in"
+          elif acc=="US":
+              way="us"
+          elif acc=="UK":
+              way="co.uk"
+          elif acc=="Canada":
+              way="ca"
+          elif acc=="Australia":
+              way="com.au"
+          elif acc=="Ireland":
+              way="ie"
+          file=gTTS(text=tex,lang='en', tld=way)
+      try:
+          loc = fd.asksaveasfilename(defaultextension="*.mp3")
+          file.save(loc)
+      except:
+          loc = "null"
+    except:
+        mb.showerror(title="Text to Speech", message="Enter Text!")
+
+def play():
+   try:
+       if loc!="null":
+         os.system("afplay "+loc)
+       else:
+           mb.showerror(title="Text to Speech", message="First Convert!")
+   except:
+       mb.showerror(title="Text to Speech", message="First Convert!")
+
+but1 = ttk.Button(root, text="play", command=play)
+but1.place(x=220,y=365)
+
+but = ttk.Button(root, text="convert",command=hit)
+but.place(x=220,y=400)
+
+enter.insert("1.0","Enter Text")
+
+say = ttk.Label(root)
+say.place(x=30, y=370)
+
+say1 = ttk.Label(root, text="[supports multiple languages]")
+say1.place(x=10, y= 404)
+
+ip = socket.gethostbyname(socket.gethostname())
+network = not ip == '127.0.0.1'
+if network==True:
+    say.config(text="internet available ✅")
+elif network==False:
+    say.config(text="internet not available ❌")
+
+say2 = ttk.Label(root,text='choose the english accent👇')
+say2.place(x=350,y=370)
+root.mainloop()
